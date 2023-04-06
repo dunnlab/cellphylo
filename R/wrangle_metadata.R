da1118185e828da31e9c00cc3bf37871f32dc0eb
Jasmine Mah <jasmine.mah@yale.edu>
2023-04-06 17:03:19 -0400

    first function

---
+++ R/wrangle_metadata.R
@@ -0,0 +1,65 @@
+wrangle_metadata <- function(path_to_meta_file){
+
+  library(tidyverse)
+  #read in meta data file
+  meta_data <- read.csv(path_to_meta_file, header=TRUE)
+  #format - remove first row
+  meta_data <- meta_data[-1,]
+
+  #remove all `'` and spaces - messes up data frame actions
+  meta_data$Cluster <- gsub("\\s", "_", meta_data$Cluster)
+  meta_data$Cluster <- gsub("'", "", meta_data$Cluster)
+
+  #save original cluster names
+  meta_data <- meta_data %>% mutate(orig_cluster_label = meta_data$Cluster)
+  colnames(meta_data) <- c("cell_identifier", "cluster", "orig_cluster_label")
+
+  #cell barcodes in metafile end in -1 but in matrix end in .1
+  #reserve "-" for cell type label delimeter
+  meta_data$cell_identifier<- gsub("-1", ".1", meta_data$cell_identifier)
+
+  #replace cell names
+  meta_data$cluster <- gsub("NK/T","NKT", meta_data$cluster)
+  meta_data$cluster  <- gsub("NK/T_cell","NKT", meta_data$cluster)
+  meta_data$cluster  <- gsub("NKT_cell","NKT", meta_data$cluster)
+  meta_data$cluster  <- gsub("Nonmyelinating_schwann_cell","SchwannCell-nmy", meta_data$cluster)
+  meta_data$cluster <- gsub("Myelinating_schwann_cell","SchwannCell-my", meta_data$cluster)
+  meta_data$cluster <- gsub("Schwann_cell","SchwannCell", meta_data$cluster)
+  meta_data$cluster  <- gsub("BeamCella", "Beam-A", meta_data$cluster)
+  meta_data$cluster  <- gsub("Beam_A", "Beam-A", meta_data$cluster)
+  meta_data$cluster  <- gsub("BeamA", "Beam-A", meta_data$cluster)
+  meta_data$cluster  <- gsub("BeamCellb","Beam-B", meta_data$cluster)
+  meta_data$cluster  <- gsub("Beam_X","Beam-X", meta_data$cluster)
+  meta_data$cluster <- gsub("Beam_Y","Beam-Y", meta_data$cluster)
+  meta_data$cluster <- gsub("Ciliary_muscle", "CiliaryMuscle", meta_data$cluster)
+  meta_data$cluster <- gsub("CribiformJCT","JCT", meta_data$cluster)
+  meta_data$cluster <- gsub("B_cell","BCell", meta_data$cluster)
+  meta_data$cluster <- gsub("VascularEndo","Endo-Vasc", meta_data$cluster)
+  meta_data$cluster <- gsub("Vascular_Endothelium","Endo-Vasc", meta_data$cluster)
+  meta_data$cluster <- gsub("ScEndo","Endo-Schlemms", meta_data$cluster)
+  meta_data$cluster <- gsub("Corneal_endothelium","Endo-Corneal", meta_data$cluster)
+  meta_data$cluster <- gsub("Endothelium","Endo", meta_data$cluster)
+  meta_data$cluster <- gsub("CornealEpi","Epi-Corneal", meta_data$cluster)
+  meta_data$cluster <- gsub("Corneal_epithelium","Epi-Corneal", meta_data$cluster)
+  meta_data$cluster <- gsub("Pigmented_ciliary_epithelium","Epi-CiliaryPigment", meta_data$cluster)
+  meta_data$cluster <- gsub("Nonpigmented_ciliary_epithelium","Epi-CiliaryNonPigment", meta_data$cluster)
+  meta_data$cluster <- gsub("Pigmented_epithelium","Epi-Pigment", meta_data$cluster)
+  meta_data$cluster <- gsub("SChlemms_Canal","SchlemmsCanal", meta_data$cluster) #typo fixed
+  meta_data$cluster <- gsub("Collector_channel","CollectorChn", meta_data$cluster)
+
+  #cluster id
+  #collapse all underscores eg. 15_Pericyte -> 15Pericyte. Humans have no cluster number.
+  cluster_id <- sub("_", "", meta_data$cluster)
+
+  #sample ID
+  sample_id <- sapply(strsplit(meta_data$cell_identifier, "_"), function(v){return(v[1])})
+  cell_barcode <- sapply(strsplit(meta_data$cell_identifier, "_"), function(v){return(v[2])})
+
+  #add annotations to meta data table
+  meta_data <- meta_data  %>% mutate(cluster_id = cluster_id, sample_id = sample_id, cell_barcode = cell_barcode)
+
+  write.table(meta_data, "all_five_species_metafile_wrangled.csv", quote=FALSE, sep = "\t", col.names=TRUE, row.names=FALSE)
+
+  return(meta_data)
+
+}
